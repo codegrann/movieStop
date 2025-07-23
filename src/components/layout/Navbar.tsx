9fd1441d97c35a0d11f591f9fe252d10695619c5
codegrann <renderer721@gmail.com>
2025-07-23 13:07:40 +0300

    .

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -47,7 +47,7 @@ const Navbar = () => {
                 Genres <ChevronDown className="w-5 h-5 ml-1" />
               </button>
               {genreDropdownOpen && (
-                <div className="absolute mt-2 w-48 bg-gray-700 rounded-md shadow-lg">
+                <div className="absolute mt-2 w-48 bg-gray-700 rounded-md shadow-lg h-64 overflow-auto custom-scrollbar">
                   {genres.map((genre) => (
                     <button
                       key={genre.id}
@@ -89,14 +89,14 @@ const Navbar = () => {
             className="md:hidden p-2 rounded hover:bg-gray-700"
             aria-label="Toggle menu"
           >
-            {sidebarOpen ? '' : <Menu className="w-6 h-6" />}
+            {sidebarOpen ? <X className="w-6 h-6" /> : <Menu className="w-6 h-6" />}
           </button>
         </div>
       </nav>
 
       {/* Sidebar for small screens */}
       <div
-        className={`fixed top-0 left-0 h-full w-64 bg-gray-900 opacity-95 text-white shadow-lg transform transition-transform duration-500 ease-in-out z-50
+        className={`fixed top-0 left-0 h-full w-64 bg-gray-900 text-white shadow-lg transform transition-transform duration-300 ease-in-out z-50
           ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'}`}
       >
         <div className="p-4 border-b border-gray-700 flex justify-between items-center">
@@ -119,6 +119,32 @@ const Navbar = () => {
               {item.label}
             </button>
           ))}
+          <div className="relative">
+              <button
+                onClick={() => setGenreDropdownOpen(!genreDropdownOpen)}
+                className="flex items-center ml-3 text-cyan-200 hover:text-cyan-500 transition"
+              >
+                Genres <ChevronDown className="w-5 h-5 ml-1" />
+              </button>
+              {genreDropdownOpen && (
+                <div className="absolute mt-2 w-48 bg-gray-700 rounded-md shadow-lg h-64 overflow-auto  custom-scrollbar">
+                  {genres.map((genre) => (
+                    <button
+                      key={genre.id}
+                      onClick={() => {
+                        selectGenre(genre);
+                        setGenreDropdownOpen(false);
+                        navigate('/');
+                      }}
+                      className="block w-full text-left px-4 py-2 text-sm text-white hover:bg-gray-600"
+                    >
+                      {genre.name}
+                    </button>
+                  ))}
+                </div>
+              )}
+            </div>
+
           {user && (
             <button
               onClick={() => {
