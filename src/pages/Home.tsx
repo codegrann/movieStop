b64996c44f6a033e806cda7574331ea19b1c050a
codegrann <renderer721@gmail.com>
2025-07-23 04:47:07 +0300

    .

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -48,13 +48,13 @@ const HomePage = () => {
     <div className="min-h-screen bg-gray-900 text-white">
       <div className="pt-12">
         {/* Hero Section */}
-        <div className="relative h-[600px] text-center py-20 md:py-32 px-4 overflow-hidden">
+        <div className="relative h-[50vh] text-center py-20 md:py-24 px-4 overflow-hidden">
           <HeroSlider movies={heroMovies} />
           <div className="relative z-10">
-            <h1 className="text-4xl md:text-5xl font-extrabold mb-4 drop-shadow-lg">
+            <h1 className="text-3xl md:text-5xl font-extrabold mb-4 drop-shadow-lg">
               Welcome to MovieStop
             </h1>
-            <p className="text-lg text-gray-200 mb-8 drop-shadow-md">
+            <p className="text-md md:text-lg text-gray-200 mb-4 sm:mb-8 drop-shadow-md">
               Discover, search, and track your favorite films.
             </p>
             <div className="mb-6 flex justify-center">
