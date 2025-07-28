fc5c042752dab1eb9c1df22af353b4775c7ce998
codegrann <renderer721@gmail.com>
2025-07-28 12:38:55 +0300

    updated CORS to allow only the frontend URL

--- services/server.ts
+++ services/server.ts
@@ -17,7 +17,11 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  })
+);
 app.use(express.json());
 app.use(passport.initialize());
 
@@ -25,11 +29,6 @@ app.use(passport.initialize());
 app.use('/api/auth', authRoutes);
 app.use('/api/movies', movieRoutes);
 app.use('/api/user', userRoutes);
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-  })
-);
 
 app.get('/', (req, res) => {
   res.send(`
